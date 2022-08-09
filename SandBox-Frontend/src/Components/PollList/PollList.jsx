ef9d271ce4b3fd6f9d05a67ff33d79eda350d980
devops <diana.behshad@gmail.com>
2022-08-09 12:39:12 +0430

    Create poll list page

--- SandBox-Frontend/src/Components/PollList/PollList.jsx
+++ SandBox-Frontend/src/Components/PollList/PollList.jsx
@@ -1,7 +1,9 @@
 import "./PollList.css";
-import PassTextFiled from "../PassTextFiled/PassTextFiled";
+import Header from "../Header/Header";
+import Card from "../Card/Card";
 import React, { useState, useEffect } from "react";
-import { TextField, Button } from "@mui/material";
+import { Fab } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
@@ -10,7 +12,60 @@ const PollList = () => {
 
   return (
     <div className="list">
-      <p>Poll list</p>
+      <Header />
+      <div className="list-container">
+        <Card
+          title={"Hangout"}
+          description={"Do you wanna hangout tonight?"}
+          img={
+            "https://images.unsplash.com/photo-1532635236-d50c8592eb08?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=811&q=80"
+          }
+          alt={"hangout"}
+        />
+        <Card
+          title={"Hangout"}
+          description={"Do you wanna hangout tonight?"}
+          img={
+            "https://images.unsplash.com/photo-1532635236-d50c8592eb08?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=811&q=80"
+          }
+          alt={"hangout"}
+        />
+        <Card
+          title={"Hangout"}
+          description={"Do you wanna hangout tonight?"}
+          img={
+            "https://images.unsplash.com/photo-1532635236-d50c8592eb08?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=811&q=80"
+          }
+          alt={"hangout"}
+        />
+        <Card
+          title={"Hangout"}
+          description={"Do you wanna hangout tonight?"}
+          img={
+            "https://images.unsplash.com/photo-1532635236-d50c8592eb08?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=811&q=80"
+          }
+          alt={"hangout"}
+        />
+        <Card
+          title={"Hangout"}
+          description={"Do you wanna hangout tonight?"}
+          img={
+            "https://images.unsplash.com/photo-1532635236-d50c8592eb08?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=811&q=80"
+          }
+          alt={"hangout"}
+        />
+        <Card
+          title={"Hangout"}
+          description={"Do you wanna hangout tonight?"}
+          img={
+            "https://images.unsplash.com/photo-1532635236-d50c8592eb08?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=811&q=80"
+          }
+          alt={"hangout"}
+        />
+        <Fab color="primary" aria-label="add">
+          <AddIcon />
+        </Fab>
+      </div>
     </div>
   );
 };
