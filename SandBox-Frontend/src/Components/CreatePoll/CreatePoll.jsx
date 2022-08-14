29072bfcdf4bbbd2b685822a1403390b5792637c
devops <diana.behshad@gmail.com>
2022-08-14 12:32:01 +0430

    Create New Poll

---
+++ SandBox-Frontend/src/Components/CreatePoll/CreatePoll.jsx
@@ -0,0 +1,182 @@
+import "./CreatePoll.css";
+import React, { useState, useEffect } from "react";
+import { TextField, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const CreatePoll = () => {
+  const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [imgURL, setImgURL] = useState("");
+  const [firstOption, setFirstOption] = useState("");
+  const [secondOption, setSecondOption] = useState("");
+  const [thirdOption, setThirdOption] = useState("");
+  const [error, setError] = useState("");
+
+  const [insertedID, setInsertedID] = useState("");
+
+  const createNewPoll = async () => {
+    if (
+      title === "" ||
+      firstOption === "" ||
+      secondOption === "" ||
+      thirdOption === ""
+    ) {
+      setError(" Title And Options Fields Can't Be Empty!");
+      return;
+    }
+    const token = localStorage.getItem("token");
+    axios
+      .post(
+        `http://localhost:3001/poll`,
+        {
+          title: title,
+          description: description,
+          img_url: imgURL,
+        },
+        {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((res) => {
+        const id = res.data[0].insertId;
+        setInsertedID(id);
+        createPollItems(insertedID);
+      })
+      .catch((res) => {
+        let error = res.response.data;
+        let status = res.response.status;
+        console.log("err :>> ", status, error);
+        if(status === 401){
+          navigate("/sign-in");
+        }
+      });
+  };
+
+  const createPollItems = async (id) => {
+    const token = localStorage.getItem("token");
+    const pollID = id;
+    console.log('pollID :>> ', pollID);
+    axios
+      .post(
+        `http://localhost:3001/item`,
+        [
+          {
+            poll_id: pollID,
+            item: firstOption,
+          },
+          {
+            poll_id: insertedID,
+            item: secondOption,
+          },
+          {
+            poll_id: insertedID,
+            item: thirdOption,
+          },
+        ],
+        {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log("res :>> ", res);
+      })
+      .catch((res) => {
+        let error = res.response.data;
+        let status = res.response.status;
+        console.log("err :>> ", status, error);
+        if(status === 401){
+          navigate("/sign-in");
+        }
+      });
+  };
+
+
+  // const pollCreator = async () => {
+  //   let pollID = await createNewPoll();
+  //   console.log('poll_id :>> ', pollID);
+  //   await createPollItems(pollID);
+  // }
+
+  return (
+    <div className="create-poll">
+      <div className="create-poll-container">
+        <h1 className="header">Create Poll</h1>
+        <TextField
+          className="text-filed"
+          id="outlined-basic"
+          label="Title"
+          variant="outlined"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <p className="create-poll-text error"></p>
+        <TextField
+          id="outlined-multiline-static"
+          label="Description"
+          multiline
+          rows={4}
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+        />
+        <p className="create-poll-text error"></p>
+        <TextField
+          className="text-filed"
+          id="outlined-basic"
+          label="Img URL"
+          variant="outlined"
+          onChange={(e) => setImgURL(e.target.value)}
+          value={imgURL}
+        />
+        <p className="create-poll-text error"></p>
+        <div className="option-container">
+          <div className="options">
+            <TextField
+              className="text-filed"
+              id="outlined-basic"
+              label="First Option"
+              variant="outlined"
+              onChange={(e) => setFirstOption(e.target.value)}
+              value={firstOption}
+            />
+          </div>
+          <div className="options">
+            <TextField
+              className="text-filed"
+              id="outlined-basic"
+              label="Second Option"
+              variant="outlined"
+              onChange={(e) => setSecondOption(e.target.value)}
+              value={secondOption}
+            />
+          </div>
+          <div className="options">
+            <TextField
+              className="text-filed"
+              id="outlined-basic"
+              label="Third Option"
+              variant="outlined"
+              onChange={(e) => setThirdOption(e.target.value)}
+              value={thirdOption}
+            />
+          </div>
+        </div>
+        <p className="create-poll-text error">{error}</p>
+        <Button
+          variant="contained"
+          className="create-poll-btn text"
+          onClick={createNewPoll}
+        >
+          Submit
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePoll;
