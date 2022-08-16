f1cb9854fa04defb40bf64903048292f5f630fbb
devops <diana.behshad@gmail.com>
2022-08-17 00:05:15 +0430

    Add create modal to create poll page

--- SandBox-Frontend/src/Components/CreatePoll/CreatePoll.jsx
+++ SandBox-Frontend/src/Components/CreatePoll/CreatePoll.jsx
@@ -1,5 +1,6 @@
 import "./CreatePoll.css";
 import Header from "../Header/Header";
+import CreatePollModal from "../CreatePollModal/CreatePollModal";
 import React, { useState, useEffect } from "react";
 import { TextField, Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
@@ -15,8 +16,7 @@ const CreatePoll = () => {
   const [thirdOption, setThirdOption] = useState("");
   const [error, setError] = useState("");
   const [name, setName] = useState("");
-  const [insertedID, setInsertedID] = useState("");
-
+  const [openModal, setOpenModal] = useState("");
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -62,15 +62,13 @@ const CreatePoll = () => {
       )
       .then((res) => {
         const id = res.data[0].insertId;
-        // setInsertedID(id);
-        // createPollItems(insertedID);
         createPollItems(id);
       })
-      .catch((res) => {
-        let error = res.response.data;
-        let status = res.response.status;
+      .catch((err) => {
+        let error = err.response.data;
+        let status = err.response.status;
         console.log("err :>> ", status, error);
-        if(status === 401){
+        if (status === 401) {
           navigate("/sign-in");
         }
       });
@@ -79,7 +77,6 @@ const CreatePoll = () => {
   const createPollItems = async (id) => {
     const token = localStorage.getItem("token");
     const pollID = id;
-    console.log('pollID :>> ', pollID);
     axios
       .post(
         `http://localhost:3001/item`,
@@ -89,11 +86,11 @@ const CreatePoll = () => {
             item: firstOption,
           },
           {
-            poll_id: insertedID,
+            poll_id: pollID,
             item: secondOption,
           },
           {
-            poll_id: insertedID,
+            poll_id: pollID,
             item: thirdOption,
           },
         ],
@@ -104,99 +101,113 @@ const CreatePoll = () => {
         }
       )
       .then((res) => {
-        console.log("res :>> ", res);
+        getNewPollLink(pollID);
       })
-      .catch((res) => {
-        let error = res.response.data;
-        let status = res.response.status;
+      .catch((err) => {
+        let error = err.response.data;
+        let status = err.response.status;
         console.log("err :>> ", status, error);
-        if(status === 401){
+        if (status === 401) {
           navigate("/sign-in");
         }
       });
   };
 
+  const getNewPollLink = async (id) => {
+    const token = localStorage.getItem("token");
+    axios
+      .get(`http://localhost:3001/poll/pollID/${id}`, {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      })
+      .then(async(res) => {
+        let link = await res.data[0].link;
+        if(link){
+          setOpenModal(link);
+        }
+      })
+      .catch((err) => {
+        console.log("err :>> ", err);
+      });
+  };
 
-  // const pollCreator = async () => {
-  //   let pollID = await createNewPoll();
-  //   console.log('poll_id :>> ', pollID);
-  //   await createPollItems(pollID);
-  // }
 
   return (
-    <div className="create-poll">
-      <Header
-      name = {name}
-      />
-      <div className="create-poll-container">
-        <h1 className="poll-headers">Create Poll</h1>
-        <TextField
-          className="text-filed"
-          id="outlined-basic"
-          label="Title"
-          variant="outlined"
-          onChange={(e) => setTitle(e.target.value)}
-          value={title}
-        />
-        <p className="poll-error-text error"></p>
-        <TextField
-          id="outlined-multiline-static"
-          label="Description"
-          multiline
-          rows={4}
-          onChange={(e) => setDescription(e.target.value)}
-          value={description}
-        />
-        <p className="poll-error-text error"></p>
-        <TextField
-          className="text-filed"
-          id="outlined-basic"
-          label="Img URL"
-          variant="outlined"
-          onChange={(e) => setImgURL(e.target.value)}
-          value={imgURL}
-        />
-        <p className="poll-error-text error"></p>
-        <div className="option-container">
-          <div className="options">
-            <TextField
-              className="text-filed"
-              id="outlined-basic"
-              label="First Option"
-              variant="outlined"
-              onChange={(e) => setFirstOption(e.target.value)}
-              value={firstOption}
-            />
+    <div className="page-container">
+      <Header name={name} />
+      <div className="create-poll">
+        <div className="create-poll-container">
+          <h1 className="poll-headers">Create Poll</h1>
+          <TextField
+            className="text-filed"
+            id="outlined-basic"
+            label="Title"
+            variant="outlined"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+          <p className="poll-error-text error"></p>
+          <TextField
+            id="outlined-multiline-static"
+            label="Description"
+            multiline
+            rows={4}
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
+          />
+          <p className="poll-error-text error"></p>
+          <TextField
+            className="text-filed"
+            id="outlined-basic"
+            label="Img URL"
+            variant="outlined"
+            onChange={(e) => setImgURL(e.target.value)}
+            value={imgURL}
+          />
+          <p className="poll-error-text error"></p>
+          <div className="option-container">
+            <div className="options">
+              <TextField
+                className="text-filed"
+                id="outlined-basic"
+                label="First Option"
+                variant="outlined"
+                onChange={(e) => setFirstOption(e.target.value)}
+                value={firstOption}
+              />
+            </div>
+            <div className="options">
+              <TextField
+                className="text-filed"
+                id="outlined-basic"
+                label="Second Option"
+                variant="outlined"
+                onChange={(e) => setSecondOption(e.target.value)}
+                value={secondOption}
+              />
+            </div>
+            <div className="options">
+              <TextField
+                className="text-filed"
+                id="outlined-basic"
+                label="Third Option"
+                variant="outlined"
+                onChange={(e) => setThirdOption(e.target.value)}
+                value={thirdOption}
+              />
+            </div>
           </div>
-          <div className="options">
-            <TextField
-              className="text-filed"
-              id="outlined-basic"
-              label="Second Option"
-              variant="outlined"
-              onChange={(e) => setSecondOption(e.target.value)}
-              value={secondOption}
-            />
-          </div>
-          <div className="options">
-            <TextField
-              className="text-filed"
-              id="outlined-basic"
-              label="Third Option"
-              variant="outlined"
-              onChange={(e) => setThirdOption(e.target.value)}
-              value={thirdOption}
-            />
-          </div>
+          <p className="poll-error-text error">{error}</p>
+          <Button
+            variant="contained"
+            className="create-poll-btn text"
+            onClick={createNewPoll}
+          >
+            Submit
+          </Button>
+          {/* <CreatePollModal link = {openModal} /> */}
         </div>
-        <p className="poll-error-text error">{error}</p>
-        <Button
-          variant="contained"
-          className="create-poll-btn text"
-          onClick={createNewPoll}
-        >
-          Submit
-        </Button>
       </div>
     </div>
   );
