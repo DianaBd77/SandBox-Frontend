101923b60101a269c70f0811ceed9a3de259585c
devops <diana.behshad@gmail.com>
2022-08-16 20:34:03 +0430

    Add poll root to app component

--- SandBox-Frontend/src/App.js
+++ SandBox-Frontend/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Home from "./Components/Home/Home";
+import Poll from "./Components/Poll/Poll";
 import Footer from "./Components/Footer/Footer";
 import SignIn from "./Components/SignIn/SignIn";
 import SignUp from "./Components/SignUp/SignUp";
@@ -14,21 +15,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <div className="app">
-      <div className="App-body">
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/sign-in" element={<SignIn />}></Route>
-            <Route path="/sign-up" element={<SignUp />}></Route>
-            <Route path="/list" element={<PollList />}></Route>
-            <Route path="/manage-poll" element={<ManagePoll />}></Route>
-            <Route path="/create-poll" element={<CreatePoll />}></Route>
-            <Route path="/panel" element={<UserPanel />}></Route>
-          </Routes>
-        </Router>
-      </div>
+        <div className="App-body">
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/poll/:id" element={<Poll />}></Route>
+              <Route path="/sign-in" element={<SignIn />}></Route>
+              <Route path="/sign-up" element={<SignUp />}></Route>
+              <Route path="/list" element={<PollList />}></Route>
+              <Route path="/manage-poll/:id" element={<ManagePoll />}></Route>
+              <Route path="/create-poll" element={<CreatePoll />}></Route>
+              <Route path="/panel" element={<UserPanel />}></Route>
+            </Routes>
+            <Navigation />
+          </Router>
+        </div>
       <Footer className="footer" />
-      <Navigation />
     </div>
   );
 };
