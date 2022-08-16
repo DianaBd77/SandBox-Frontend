fcfee4dd020bc6dbb2324cc9aa5da62ebdf8bb2c
devops <diana.behshad@gmail.com>
2022-08-16 23:56:07 +0430

    Add delete poll modal

---
+++ SandBox-Frontend/src/Components/DeleteModal/DeleteModal.jsx
@@ -0,0 +1,138 @@
+import "./DeleteModal.css";
+import axios from "axios";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { Button } from "@mui/material";
+import { Delete } from "@mui/icons-material";
+import { Close } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+
+export default function DeleteModal({ link, id }) {
+  const navigate = useNavigate();
+  let pollLink = `${link}`;
+  let pollID = id;
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const deletePoll = () => {
+    const token = localStorage.getItem("token");
+    axios
+      .delete(`http://localhost:3001/poll/${pollLink}`, {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        console.log("res :>> ", res);
+        deletePollData(pollID);
+        handleClose();
+      })
+      .catch((err) => {
+        let status = err.response.status;
+        if (status === 401) {
+          navigate("/sign-in");
+        }
+      });
+  };
+
+  const deletePollData = async (id) => {
+    const token = localStorage.getItem("token");
+
+    const deleteItems = axios.delete(`http://localhost:3001/item/${id}`, {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+
+    const deleteParticipants = axios.delete(
+      `http://localhost:3001/participant/${id}`,
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    const deleteChoices = axios.delete(`http://localhost:3001/choice/${id}`, {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+
+    await axios.all([deleteItems, deleteParticipants, deleteChoices]).then(axios.spread(function(resItem, resParticipant, resChoice){
+      console.log('resItem :>> ', resItem);
+      console.log('resParticipant :>> ', resParticipant);
+      console.log('resChoice :>> ', resChoice);
+    }))
+      .catch((err) => {
+        console.log(err);
+      });
+
+    // axios
+    //   .delete(
+    //     `http://localhost:3001/poll/${id}`,
+    //     {
+    //       headers: {
+    //         authorization: `Bearer ${token}`,
+    //       },
+    //     }
+    //   )
+    //   .then((res) => {
+    //     console.log("res :>> ", res);
+
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+  };
+
+  return (
+    <div>
+      <Delete className="card-icons" onClick={handleOpen} />
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box className={"modal-box"}>
+          <Close className="close-delete-link" onClick={handleClose} />
+          <Typography
+            className="delete-link-header"
+            id="modal-modal-title"
+            variant="h6"
+            component="h2"
+          >
+            Delete Poll
+          </Typography>
+          <Typography
+            className="delete-link-text"
+            id="modal-modal-description"
+            sx={{ mt: 2 }}
+          >
+            Are you sure you want to delete this poll?
+          </Typography>
+          <div className="delete-link-button">
+            <Button
+              variant="outlined"
+              className="cancel-poll-btn delete-button"
+              onClick={handleClose}
+            >
+              Cancel
+            </Button>
+            <Button
+              variant="contained"
+              className="delete-poll-btn delete-button"
+              onClick={deletePoll}
+            >
+              Delete
+            </Button>
+          </div>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
