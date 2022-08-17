e62182d441746bbde9a87c26deb39909dc0c8b61
devops <diana.behshad@gmail.com>
2022-08-17 13:02:45 +0430

    Add poll link page

--- SandBox-Frontend/src/App.js
+++ SandBox-Frontend/src/App.js
@@ -4,6 +4,7 @@ import Poll from "./Components/Poll/Poll";
 import Footer from "./Components/Footer/Footer";
 import SignIn from "./Components/SignIn/SignIn";
 import SignUp from "./Components/SignUp/SignUp";
+import PollLink from "./Components/PollLink/PollLink";
 import PollList from "./Components/PollList/PollList";
 import UserPanel from "./Components/UserPanel/UserPanel";
 import ManagePoll from "./Components/ManagePoll/ManagePoll";
@@ -15,21 +16,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <div className="app">
-        <div className="App-body">
-          <Router>
-            <Routes>
-              <Route path="/" element={<Home />}></Route>
-              <Route path="/poll/:id" element={<Poll />}></Route>
-              <Route path="/sign-in" element={<SignIn />}></Route>
-              <Route path="/sign-up" element={<SignUp />}></Route>
-              <Route path="/list" element={<PollList />}></Route>
-              <Route path="/manage-poll/:id" element={<ManagePoll />}></Route>
-              <Route path="/create-poll" element={<CreatePoll />}></Route>
-              <Route path="/panel" element={<UserPanel />}></Route>
-            </Routes>
-            <Navigation />
-          </Router>
-        </div>
+      <div className="App-body">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/poll/:id" element={<Poll />}></Route>
+            <Route path="/sign-in" element={<SignIn />}></Route>
+            <Route path="/sign-up" element={<SignUp />}></Route>
+            <Route path="/list" element={<PollList />}></Route>
+            <Route path="/manage-poll/:id" element={<ManagePoll />}></Route>
+            <Route path="/create-poll" element={<CreatePoll />}></Route>
+            <Route path="/panel" element={<UserPanel />}></Route>
+            <Route path="/poll-link/:id" element={<PollLink />}></Route>
+          </Routes>
+          <Navigation />
+        </Router>
+      </div>
       <Footer className="footer" />
     </div>
   );
