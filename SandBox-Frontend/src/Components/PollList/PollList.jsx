ea5e6e1b4c24d09ff281d9b1abd72e87967ef95d
devops <diana.behshad@gmail.com>
2022-08-18 15:33:31 +0430

    Remove pollData hook and render inside poll list

--- SandBox-Frontend/src/Components/PollList/PollList.jsx
+++ SandBox-Frontend/src/Components/PollList/PollList.jsx
@@ -1,16 +1,41 @@
 import "./PollList.css";
 import Header from "../Header/Header";
 import Card from "../Card/Card";
-import usePollData from "./usePollData";
+import React, { useState, useEffect } from "react";
 import useUsername from "../Header/useUsername";
 import { Fab } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const PollList = () => {
   const navigate = useNavigate();
-  const [pollData, fetchData] = usePollData();
+  // const [pollData, fetchData] = usePollData();
+  const [pollData, setPollData] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
   const [name] = useUsername();
+
+  const fetchData = () => {
+    const token = localStorage.getItem("token");
+    axios
+      .get(`http://localhost:3001/poll`, {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        const data = response.data;
+        setPollData(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        setErrorMessage(error.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   let poll = pollData.map((data) => {
     return (
