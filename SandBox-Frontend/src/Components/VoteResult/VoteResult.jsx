8ccb74359487ed2509074a28c808efa622e29d59
devops <diana.behshad@gmail.com>
2022-08-18 15:32:05 +0430

    Add vote result component

---
+++ SandBox-Frontend/src/Components/VoteResult/VoteResult.jsx
@@ -0,0 +1,65 @@
+import "./VoteResult.css";
+import React, { useState } from "react";
+import { Done, Close } from "@mui/icons-material";
+
+const VoteResult = () => {
+  const [participantName, setParticipantName] = useState("");
+
+  return (
+    <div className="vote-result-card">
+      <div className="poll-vote-result-box">
+        <div className="poll-participant-name-container">
+          <div className="vote-participant-name-container">
+            <div className="poll-item-title-box vote-participant-name-box">
+              <p>Diana</p>
+            </div>
+          </div>
+          <div className="poll-participant-name-box"></div>
+        </div>
+        <div className="poll-result-container">
+          <div className="poll-item-title-container">
+            <div className="poll-item-title-box participant-vote-result">
+              <div className="poll-item-title-text">
+                <Done
+                  sx={{ width: "40px", height: "40px", color: "#381e72" }}
+                />
+              </div>
+            </div>
+            <div
+              className="poll-item-title-box participant-vote-result"
+              style={{ backgroundColor: "#F6EDFF" }}
+            >
+              <div className="poll-item-title-text">
+                <Close
+                  sx={{
+                    width: "40px",
+                    height: "40px",
+                    color: "black",
+                    opacity: "5%",
+                  }}
+                />
+              </div>
+            </div>
+            <div
+              className="poll-item-title-box participant-vote-result"
+              style={{ backgroundColor: "#F6EDFF" }}
+            >
+              <div className="poll-item-title-text">
+                <Close
+                  sx={{
+                    width: "40px",
+                    height: "40px",
+                    color: "black",
+                    opacity: "5%",
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VoteResult;
